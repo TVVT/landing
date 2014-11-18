05a10546796ac86fe260d51b1a8c035cac57ae5c
kevin14 <kevin14@me.com>
2014-11-18 15:28:49 +0800

    增加调整白平衡以及拍照停止的功能

--- iOS/Tizen/Tizen/ViewController.swift
+++ iOS/Tizen/Tizen/ViewController.swift
@@ -14,22 +14,50 @@ import AVFoundation
 class ViewController: UIViewController {
     let captureSession = AVCaptureSession()
     
+    var timer:NSTimer?
     
-//    @IBAction func photoBtn(sender: AnyObject) {
-//        
-//        takePhoto()
-//        
-//    }
+    @IBAction func photoBtn(sender: AnyObject) {
+        if let timer = self.timer{
+            timer.invalidate()
+            self.timer = nil
+            println(self.timer?.valid)
+        }else{
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.32, target: self, selector: Selector("takePhoto"), userInfo: nil, repeats: true)
+            println(self.timer?.valid)
+        }
+    }
+    @IBOutlet var settingButton: UIButton!
+    @IBOutlet var photoButton: UIButton!
+    @IBOutlet var settingView: UIView!
+    @IBOutlet var rSlider: UISlider!
+    @IBOutlet var gSlider: UISlider!
+    @IBOutlet var bSlider: UISlider!
+    
+    @IBOutlet var rLabel: UILabel!
+    @IBOutlet var gLabel: UILabel!
+    @IBOutlet var bLabel: UILabel!
+    
+    
+    @IBAction func setParams(sender: AnyObject) {
+        if settingView.hidden == true{
+            settingView.hidden = false
+        }else{
+            settingView.hidden = true
+        }
+    }
     // If we find a device we'll store it here for later use
     var captureDevice : AVCaptureDevice?
     var input : AVCaptureInput?
-
-
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initLabels()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        settingButton.layer.zPosition = 100
+        photoButton.layer.zPosition = 100
+        settingView.layer.zPosition = 100
 
         captureSession.sessionPreset = AVCaptureSessionPresetHigh;
         let devices = AVCaptureDevice.devices();
@@ -56,6 +84,50 @@ class ViewController: UIViewController {
         
     }
 
+    func initLabels(){
+        rSlider.minimumValue = 1
+        rSlider.maximumValue = 4
+        
+        gSlider.minimumValue = 1
+        gSlider.maximumValue = 4
+        
+        bSlider.minimumValue = 1
+        bSlider.maximumValue = 4
+        
+        rLabel.text = NSString(format: "%.02f",rSlider.value)
+        gLabel.text = NSString(format: "%.02f",gSlider.value)
+        bLabel.text = NSString(format: "%.02f",bSlider.value)
+        
+    }
+    
+    
+    //actions
+    @IBAction func rSetter(sender: AnyObject) {
+        rLabel.text = NSString(format: "%.02f",rSlider.value)
+        changeWhiteBalance(r: rSlider.value,g: gSlider.value,b: bSlider.value)
+    }
+ 
+    @IBAction func gSetter(sender: AnyObject) {
+        gLabel.text = NSString(format: "%.02f",gSlider.value)
+        changeWhiteBalance(r: rSlider.value,g: gSlider.value,b: bSlider.value)
+    }
+    @IBAction func bSetter(sender: UISlider) {
+        bLabel.text = NSString(format: "%.02f",bSlider.value)
+        changeWhiteBalance(r: rSlider.value,g: gSlider.value,b: bSlider.value)
+    }
+    
+    
+    func changeWhiteBalance(#r:Float,g:Float,b:Float){
+        if let device = captureDevice{
+            device.lockForConfiguration(nil)
+//            device.focusMode = AVCaptureFocusMode.Locked
+//            device.whiteBalanceMode = AVCaptureWhiteBalanceMode.Locked
+            device.setWhiteBalanceModeLockedWithDeviceWhiteBalanceGains(AVCaptureWhiteBalanceGains(redGain: r,greenGain: g,blueGain: b), completionHandler: nil)
+            device.unlockForConfiguration()
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -111,7 +183,6 @@ class ViewController: UIViewController {
             device.setWhiteBalanceModeLockedWithDeviceWhiteBalanceGains(AVCaptureWhiteBalanceGains(redGain: 2.75,greenGain: 1.4,blueGain: 2.7), completionHandler: nil)
             
             println(device.ISO);
-            
             device.unlockForConfiguration()
         }
         
@@ -136,8 +207,6 @@ class ViewController: UIViewController {
         self.view.layer.addSublayer(previewLayer)
         previewLayer?.frame = CGRect(x: 0, y: 0, width: self.view.layer.frame.width, height: self.view.layer.frame.height)
         captureSession.startRunning()
-        
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.32, target: self, selector: Selector("takePhoto"), userInfo: nil, repeats: true)
 
     }
     
